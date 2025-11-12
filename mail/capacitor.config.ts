5b599a00d1485034d84042fc175f0ff95faa4fd9
matthewcumbo <matthew.abraham.cumbo@mcast.edu.mt>
2025-11-12 07:09:54 +0100

    Created new aop using SideMenu template

---
+++ mail/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mail',
+  webDir: 'www'
+};
+
+export default config;
